ccda371ed24dae238534dd1e3954e41e5f05e797
jychoi0918 <jychoi091808@gmail.com>
2021-06-19 20:08:02 +0900

    Add CPR프로젝트 생성

---
+++ redHome/src/main/java/com/CPR/redHome/RedHomeApplication.java
@@ -0,0 +1,13 @@
+package com.CPR.redHome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedHomeApplication.class, args);
+	}
+
+}
---
+++ redHome/src/test/java/com/CPR/redHome/RedHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CPR.redHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
