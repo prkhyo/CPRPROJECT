5a62eb10ed2d4215d907db6f44add8e5110d338f
prkhyo <hyojin960611@gmail.com>
2021-06-24 02:33:08 +0900

    Add Community view, creteria, pagination, paging function

--- src/main/java/com/CPR/redHome/controller/community/CommunityController.java
+++ src/main/java/com/CPR/redHome/controller/community/CommunityController.java
@@ -1,4 +1,66 @@
 package com.CPR.redHome.controller.community;
 
+import com.CPR.redHome.dto.community.CommunityDto;
+import com.CPR.redHome.paging.Criteria;
+import com.CPR.redHome.paging.Pagination;
+import com.CPR.redHome.service.community.CommunityService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+/*@RequestMapping("/community")*/
 public class CommunityController {
+
+
+    private final CommunityService communityService;
+
+
+    @GetMapping("/community/list")
+    public String communityListPage(@ModelAttribute("criteria")Criteria criteria, Model model/*, @ModelAttribute String reply, @ModelAttribute String orderType */) {
+       /* @ModelAttribute를 이용하면 파라미터로 전달받은 객체를 자동으로 뷰까지 전달*/
+
+        List<CommunityDto> communityList = Collections.emptyList();
+
+        int communityTotalCnt = communityService.countAllCommunities();
+
+        Pagination pagination = new Pagination(criteria);
+        pagination.setTotalRecordCount(communityTotalCnt);
+
+
+        HashMap<String,Object> map = new HashMap<String,Object>();
+        map.put("recordsPerPage",criteria.getRecordsPerPage());
+        map.put("firstRecordIndex",pagination.getFirstRecordIndex());
+
+       /* map.put("orderType",orderType);
+        map.put("reply",reply);
+        map.put("pageNum",cri.getPageNum());
+        map.put("amount",cri.getAmount());
+        map.put("type",cri.getType());
+        map.put("keyword",cri.getKeyword());*/
+
+
+      if(communityTotalCnt > 0){
+          communityList = communityService.getCommunityList(map);
+       }
+
+        System.out.println(communityList);//
+
+
+        model.addAttribute("communityList", communityList);
+        model.addAttribute("pageMaker",pagination);
+
+        model.addAttribute("orderType","newCommunity");//
+        model.addAttribute("reply","replyState_all");//
+
+        return "community/community_list";
+    }
+
 }
---
+++ src/main/java/com/CPR/redHome/service/community/CommunityServiceImpl.java
@@ -0,0 +1,40 @@
+package com.CPR.redHome.service.community;
+
+import com.CPR.redHome.dto.community.CommunityDto;
+import com.CPR.redHome.mapper.community.CommunityMapper;
+import com.CPR.redHome.paging.Criteria;
+import com.CPR.redHome.paging.Pagination;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CommunityServiceImpl implements CommunityService {
+
+    private final CommunityMapper communityMapper;
+
+
+    @Override
+    public int countAllCommunities() {
+        int CommunityCnt = communityMapper.selectCommunityTotalCnt();
+        return CommunityCnt;
+    }
+
+    @Override
+    public List<CommunityDto> getCommunityList(HashMap<String,Object> map) {
+
+        List<CommunityDto> communityList = communityMapper.selectAllCommunities(map);
+
+       /* String orderType = (String) map.get("orderType");
+        if (orderType.equals("newAnswer")) {
+           // communityList = boardRepository.getBoardPagingNewAnswer(map);
+            System.out.println("최근 답변순 정렬");
+        }*/
+
+        return communityList;
+    }
+}
