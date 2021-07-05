ecc40a92e4ca0e42df08bb63ca79fc36d52dcd2f
jychoi0918 <jychoi091808@gmail.com>
2021-07-05 22:18:11 +0900

    Add LogInterceptor and LoginCheckInterceptor
    인터셉터 추가(로그인 후 사용가능한 페이지들 redirect)

---
+++ src/main/java/com/CPR/redHome/web/WebConfig.java
@@ -0,0 +1,39 @@
+package com.CPR.redHome.web;
+
+import com.CPR.redHome.web.interceptor.LogInterceptor;
+import com.CPR.redHome.web.interceptor.LoginCheckInterceptor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**","/*.ico","/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/","/login","/logout","/join",
+                        "/css/**","/*.ico","/error");
+    }
+
+
+
+
+
+
+
+
+
+}
