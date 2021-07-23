b8aab2716a1d92069010e5c2d076705378e35b0a
prkhyo <hyojin960611@gmail.com>
2021-07-23 23:28:18 +0900

    Refactor separate top.html and bottom.html  from main.html

---
+++ src/main/resources/static/js/main/top.js
@@ -0,0 +1,12 @@
+function myOn(){
+    document.getElementById("popup").style['display'] = 'block';
+
+}
+
+function myOut(){
+    document.getElementById("popup").style['display'] = 'none';
+}
+
+function logOut(){
+    location.href="/logOut";
+}
