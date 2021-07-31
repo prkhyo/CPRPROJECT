3b098a5625aac76ad9570377c2d163dcf034865a
jychoi0918 <jychoi091808@gmail.com>
2021-07-31 21:47:06 +0900

    Add ReviewDto and ReviewMapper

--- src/main/java/com/CPR/redHome/mapper/order/OrderMapper.java
+++ src/main/java/com/CPR/redHome/mapper/order/OrderMapper.java
@@ -3,6 +3,7 @@ package com.CPR.redHome.mapper.order;
 import com.CPR.redHome.dto.order.OrderedDto;
 import com.CPR.redHome.paging.Criteria;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -12,11 +13,10 @@ public interface OrderMapper {
     //구매 내역을 가져오거나 조회가능 한지 구현
 
     //모든 구매내역 가져오기
-    List<OrderedDto> selectAllOrdersByMemberId(@RequestParam Long memberId,@RequestParam Criteria criteria,@RequestParam int firstRecordIndex);
+    List<OrderedDto> selectAllOrdersByMemberId(@Param("memberId") Long memberId, @Param("criteria") Criteria criteria, @Param("firstRecordIndex") int firstRecordIndex);
 
     //구매 목록 세기
     Integer countOrders(Long memberId);
-
 
 
 }
